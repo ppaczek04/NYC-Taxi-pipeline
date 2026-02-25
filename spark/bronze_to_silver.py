defd494720bf8e0db506b3fa93ed84b6ee9af808
Piotr Paczek <ppaczek1@o2.pl>
2026-02-25 21:40:43 +0100

    silver layer prep

---
+++ spark/bronze_to_silver.py
@@ -0,0 +1,43 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, current_timestamp, when
+
+BRONZE_PATH = "/app/lake/bronze/rides"
+SILVER_PATH = "/app/lake/silver/rides"
+CHECKPOINT_PATH = "/app/lake/checkpoint_silver/rides"
+
+def build_spark():
+    return (SparkSession.builder
+        .appName("NYC-Bronze-To-Silver")
+        .master("spark://spark-master:7077")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .getOrCreate())
+
+def main():
+    spark = build_spark()
+    
+    # load as Stream (even if its batch)
+    bronze_df = spark.readStream.format("delta").load(BRONZE_PATH)
+
+    # cleaning
+    silver_df = bronze_df.filter(
+        (col("passenger_count") > 0) & 
+        (col("trip_distance") > 0.0) &
+        (col("fare_amount") > 0.0)
+    ).withColumn(
+        "is_long_trip", when(col("trip_distance") > 10, True).otherwise(False)
+    ).withColumn(
+        "_silver_ingest_ts", current_timestamp()
+    )
+
+    # saving to silver
+    query = (silver_df.writeStream
+        .format("delta")
+        .outputMode("append")
+        .option("checkpointLocation", CHECKPOINT_PATH)
+        .start(SILVER_PATH))
+
+    query.awaitTermination()
+
+if __name__ == "__main__":
+    main()
